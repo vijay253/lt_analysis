3fc1a5ae2dbd35fbf9ab583834e7c0541d601c06
Richard Trotta <trotta@cua.edu>
2023-01-03 10:54:00 -0500

    Testing kaonlt files

--- src/root_ana/analysis.cpp
+++ src/root_ana/analysis.cpp
@@ -1116,7 +1116,7 @@ void Analysis::Missing_Mass_Plot() {
 	coin_all  = new TH1F("all", "all",   coin_bin, coin_min, coin_max);
 	coin_real = new TH1F("real", "real", coin_bin, coin_min, coin_max);
 	coin_rand = new TH1F("rand", "rand", coin_bin, coin_min, coin_max);
-	cout << "aaaaaaaaaaaaaaaasssssssbbbbbbbbbbbbb" << endl;
+
 	/*-------------------------------------------------------------------
 	  REMOVED FOR KAONLT TESTING
 	  -------------------------------------------------------------------
@@ -1128,7 +1128,7 @@ void Analysis::Missing_Mass_Plot() {
 	data_tree_in->Draw("CTime_epCoinTime_ROC1>>all",  t_cut, "goff");
 	data_tree_in->Draw("CTime_epCoinTime_ROC1>>real", t_cut, "goff");
 	data_tree_in->Draw("CTime_epCoinTime_ROC1>>rand", t_cut, "goff");
-
+	cout << "aaaaaaaaaaaaaaaasssssssbbbbbbbbbbbbb" << endl;
 
 // 	cout << kset << "   " << miss_mass_offset[kset] << "   " << miss_mass_offset[1] << endl;
 // 	cout << all_coin_cut << endl;
