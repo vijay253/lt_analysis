06633755e854b0a726a60ba72aeb99c5522c9ad9
Richard Trotta <trotta@cua.edu>
2023-01-04 11:01:38 -0500

    Testing

--- src/average_kinematics.f
+++ src/average_kinematics.f
@@ -104,7 +104,7 @@ c     Get low, high eps. and neg., pos. polarity data.
             open(55,file='./root_ana/list.settings')
             do while(.true.)
 
-               read(55,*,end=9) ipol,q2,eps,th_pq,tmn,tmx,nbt
+!               read(55,*,end=9) ipol,q2,eps,th_pq,tmn,tmx,nbt
                if(ipol.eq.pol_set(ip).and.q2.eq.q2_set.and.
      &              eps.eq.eps_set(lh)) then
 
