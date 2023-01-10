19924886c011edcd3820078ff511968cb171a0cc
Richard Trotta <trotta@cua.edu>
2023-01-10 11:40:27 -0500

    Testing

--- src/root_ana/analysis.cpp
+++ src/root_ana/analysis.cpp
@@ -580,7 +580,7 @@ void Analysis::Missing_Mass_Plot() {
 	data_tree_in->Draw("CTime_ROC1>>all",  t_cut, "goff");
 	//data_tree_in->Draw("CTime_ROC1>>real", t_cut, "goff"); // HERE
 	//data_tree_in->Draw("CTime_ROC1>>rand", t_cut, "goff"); // HERE
-	cout << "TEST¬¬¬¬¬¬¬¬¬¬¬¬¬`" << endl;	
+
 // 	cout << kset << "   " << miss_mass_offset[kset] << "   " << miss_mass_offset[1] << endl;
 // 	cout << all_coin_cut << endl;
  
