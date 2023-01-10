f7ce4600cc0a9aec5bf460e02f5cdb0e81d8a51c
Richard Trotta <trotta@cua.edu>
2023-01-10 11:17:39 -0500

    Testing

--- src/root_ana/analysis.cpp
+++ src/root_ana/analysis.cpp
@@ -633,7 +633,8 @@ void Analysis::Missing_Mass_Plot() {
 	data_tree_in->Draw("MM- " + expected_mm_str + " >> mm_1", rand_coin_cut, "goff");
 	-------------------------------------------------------------------*/
 	//data_tree_in->Draw("MM- " + expected_mm_str + " >> mm");
-	data_tree_in->Draw("MM >> mm", t_cut, "goff");
+	//data_tree_in->Draw("MM >> mm", t_cut, "goff");
+	data_tree_in->Draw("MM >> mm");
 	
 	//data_tree_in->Draw("MM- " + expected_mm_str + " >> mm_1"); // HERE
 	
