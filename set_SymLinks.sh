27ddba2af564cd2e738d49cb813d74f5508ed340
Richard Trotta <trotta@cua.edu>
2023-01-14 13:33:51 -0500

    Added simc_gfortran analysis scripts. Fixed symlinks for current enviroment pathing

--- set_SymLinks.sh
+++ set_SymLinks.sh
@@ -26,10 +26,8 @@ HOST=`echo ${PATHFILE_INFO} | cut -d ','  -f15`
 SIMCPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f16`
 LTANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f17`
 
-
 # Check symlinks and create/fix if bad
 if [ ! -L "${LTANAPATH}/simc_gfortran" ]; then
-    echo ${LTANAPATH}
     ln -s "${SIMCPATH}" "${LTANAPATH}/simc_gfortran"
 elif [ -L "${LTANAPATH}/simc_gfortran" ]; then
     if [ ! -e "${LTANAPATH}/simc_gfortran" ]; then
