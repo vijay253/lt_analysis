b11ec67d1075deabb5712bfe994a062e5bcf4ece
Richard Trotta <trotta@cua.edu>
2023-01-14 14:52:23 -0500

    Removed lists unused

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-14 14:26:14 trottar"
+# Time-stamp: "2023-01-14 14:51:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -75,58 +75,67 @@ LTANAPATH=lt.LTANAPATH
 
 ################################################################################################################################################
 
+def write_to_file(f_out,line):
+    # Open a file in append mode
+    with open(f_out, 'a') as f:
+        # Write the value of the variable to the file
+        f.write(line)
+
+################################################################################################################################################
+
 # Define thpq vector relative to middle setting
 thpq_right = -abs(float(pThetaValCenter[0])-float(pThetaValRight[0]))
 thpq_left = abs(float(pThetaValCenter[0])-float(pThetaValLeft[0]))
 thpq_center = 0.000
 
-# Open a file in write mode
-with open('{}/src/root_ana/list.settings'.format(LTANAPATH), 'a') as f:
+f_list_settings = '{}/src/root_ana/list.settings'.format(LTANAPATH)
+# First check if line exists
+with open(f_list_settings, 'r') as f:
+    lines = f.readlines()
     if float(runNumRight[0]) != 0.0:
-        # Write the value of the variable to the file
-        f.write("{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_right,TMIN,TMAX,NumtBins,Kset))
+        check_line = "{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_right,TMIN,TMAX,NumtBins,Kset)
+        # Check if the line already exists
+        if check_line not in lines:
+            write_to_file(f_list_settings,check_line)
     if float(runNumLeft[0]) != 0.0:
-        f.write("{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_left,TMIN,TMAX,NumtBins,Kset))
+        check_line = "{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_left,TMIN,TMAX,NumtBins,Kset)
+        if check_line not in lines:
+            write_to_file(f_list_settings,check_line)
     if float(runNumCenter[0]) != 0.0:
-        f.write("{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_center,TMIN,TMAX,NumtBins,Kset))
+        check_line = "{} {} {} {:.3f} {} {} {} {}\n".format(POL,Q2,EPSVAL,thpq_center,TMIN,TMAX,NumtBins,Kset)
+        if check_line not in lines:
+            write_to_file(f_list_settings,check_line)
 
 if TargetType == "dummy":
-    # Open a file in write mode
-    with open('{}/src/lists/list.dummy_{}_{:.0f}'.format(LTANAPATH,Q2.replace(".",""),float(EPSVAL)*100), 'a') as f:
-        if float(runNumRight[0]) != 0.0:
-            # Write the value of the variable to the file
-            for i,thpq in enumerate(EbeamValRight):
-                # convert uC to C (10^-6C=1uC)
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumRight[i],Q2,EbeamValRight[i],float(ChargeValRight[i])/1000000,ChargeErrRight[i], \
-                                float(EffValRight[i]),EffErrRight[i],EPSVAL,thpq_right))
-        if float(runNumLeft[0]) != 0.0:
-            for i,thpq in enumerate(EbeamValLeft):
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumLeft[i],Q2,EbeamValLeft[i],float(ChargeValLeft[i])/1000000,ChargeErrLeft[i], \
-                                float(EffValLeft[i]),EffErrLeft[i],EPSVAL,thpq_left))
-        if float(runNumCenter[0]) != 0.0:
-            for i,thpq in enumerate(EbeamValCenter):
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumCenter[i],Q2,EbeamValCenter[i],float(ChargeValCenter[i])/1000000,ChargeErrCenter[i], \
-                                float(EffValCenter[i]),EffErrCenter[i],EPSVAL,thpq_center)) 
+    f_list = '{}/src/lists/list.dummy_{}_{:.0f}'.format(LTANAPATH,Q2.replace(".",""),float(EPSVAL)*100)
 else:
-    # Open a file in write mode
-    with open('{}/src/lists/list.{}_{:.0f}'.format(LTANAPATH,Q2.replace(".",""),float(EPSVAL)*100), 'a') as f:
-        if float(runNumRight[0]) != 0.0:
-            # Write the value of the variable to the file
-            for i,thpq in enumerate(EbeamValRight):
-                # convert uC to C (10^-6C=1uC)
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumRight[i],Q2,EbeamValRight[i],float(ChargeValRight[i])/1000000,ChargeErrRight[i], \
-                                float(EffValRight[i]),EffErrRight[i],EPSVAL,thpq_right))
-        if float(runNumLeft[0]) != 0.0:
-            for i,thpq in enumerate(EbeamValLeft):
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumLeft[i],Q2,EbeamValLeft[i],float(ChargeValLeft[i])/1000000,ChargeErrLeft[i], \
-                                float(EffValLeft[i]),EffErrLeft[i],EPSVAL,thpq_left))
-        if float(runNumCenter[0]) != 0.0:
-            for i,thpq in enumerate(EbeamValCenter):
-                f.write("{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
-                        .format(runNumCenter[i],Q2,EbeamValCenter[i],float(ChargeValCenter[i])/1000000,ChargeErrCenter[i], \
-                                float(EffValCenter[i]),EffErrCenter[i],EPSVAL,thpq_center)) 
+    f_list = '{}/src/lists/list.{}_{:.0f}'.format(LTANAPATH,Q2.replace(".",""),float(EPSVAL)*100)
+
+    
+# Open a file in write mode
+with open(f_list, 'r') as f:
+    lines = f.readlines()
+    if float(runNumRight[0]) != 0.0:
+        # Write the value of the variable to the file
+        for i,thpq in enumerate(EbeamValRight):
+            # convert uC to C (10^-6C=1uC)
+            check_line = "{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
+                    .format(runNumRight[i],Q2,EbeamValRight[i],float(ChargeValRight[i])/1000000,ChargeErrRight[i], \
+                            float(EffValRight[i]),EffErrRight[i],EPSVAL,thpq_right)
+            # Check if the line already exists
+            if check_line not in lines:
+                write_to_file(f_list,check_line)
+    if float(runNumLeft[0]) != 0.0:
+        for i,thpq in enumerate(EbeamValLeft):
+            check_line = "{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
+                    .format(runNumLeft[i],Q2,EbeamValLeft[i],float(ChargeValLeft[i])/1000000,ChargeErrLeft[i], \
+                            float(EffValLeft[i]),EffErrLeft[i],EPSVAL,thpq_left)
+            if check_line not in lines:
+                write_to_file(f_list,check_line)
+    if float(runNumCenter[0]) != 0.0:
+        for i,thpq in enumerate(EbeamValCenter):
+            check_line = "{} {} {} {} {} {:.5f} {} {} {:.3f}\n" \
+                    .format(runNumCenter[i],Q2,EbeamValCenter[i],float(ChargeValCenter[i])/1000000,ChargeErrCenter[i], \
+                            float(EffValCenter[i]),EffErrCenter[i],EPSVAL,thpq_center)
+            if check_line not in lines:
+                write_to_file(f_list,check_line)
