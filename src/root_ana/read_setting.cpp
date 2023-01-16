90b12cf2d7fd829c4b79978e91089b7dcbd154ea
Richard Trotta <trotta@cua.edu>
2023-01-16 18:04:10 -0500

    Testing simc

--- src/root_ana/read_setting.cpp
+++ src/root_ana/read_setting.cpp
@@ -69,7 +69,7 @@ ReadFile::ReadFile(TString eff_file_name_tmp, TString off_file_name_tmp) {
 	//off_file_name = off_file_name_tmp;
 
 	Read_init();
-	Setting_file_loading_name();
+	//Setting_file_loading_name();
 	Kin_Pro_Array_Load();
 	cout << "!~~~~~~~~~~~~~~~~~" << endl;
 	
