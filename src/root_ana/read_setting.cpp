82d458bd8659722fb99ea90c75347561610f0b82
Richard Trotta <trotta@cua.edu>
2023-01-16 18:10:25 -0500

    Testing simc

--- src/root_ana/read_setting.cpp
+++ src/root_ana/read_setting.cpp
@@ -97,7 +97,7 @@ ReadFile::ReadFile(TString simc_file_name_tmp) {
 
 	simc_file_name = simc_file_name_tmp;
 	cout << "!!~~~~~~~~~~~~~~~~~~~" << simc_file_name << endl;
-	Simc_Pro_Array_Load();
+	//Simc_Pro_Array_Load();
 
 //	cout << "Angle Check "<< simc_pro.hms_theta[2] << endl;
 //	exit(0);
@@ -147,7 +147,7 @@ void ReadFile::Setting_file_loading() {
   ///*--------------------------------------------------*/
   // Read Simulated data information file
   /*--------------------------------------------------*/
-  simc_ntp->ReadFile(simc_file_name);
+  //simc_ntp->ReadFile(simc_file_name);
 
 }
 
