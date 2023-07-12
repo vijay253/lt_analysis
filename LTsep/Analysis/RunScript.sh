2d21300034851b92e2e4b8b147d3f3d0ddab8b18
Vijay Kumar <vijay@ifarm1901.jlab.org>
2023-07-12 15:59:19 -0400

    files updated

---
+++ LTsep/Analysis/RunScript.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+echo ""
+echo "Runnning for the HMS Calorimeter efficiency study"
+
+if [[ "${HOSTNAME}" = *"ifarm"* ]]; then  
+    REPLAYPATH="/group/c-kaonlt/USERS/${USER}/hallc_replay_lt"                         
+    cd "$REPLAYPATH"
+#    source "$REPLAYPATH/setup.sh"    
+    source "/group/c-kaonlt/USERS/${USER}/hallc_replay_lt/setup.sh"    
+fi
+
+if [[ "${HOSTNAME}" != *"ifarm"* ]]; then
+    source /site/12gev_phys/softenv.sh 2.3
+    cd "/group/c-kaonlt/hcana/"
+    source "/group/c-kaonlt/hcana/setup.sh"
+    cd "$REPLAYPATH"
+    source "$REPLAYPATH/setup.sh"
+fi
+echo ""
+echo ""
+echo ""
+
+echo ""
+echo "Processing data..."
+while IFS='' read -r Line || [[ -n "$Line" ]]; do
+    RUNNUMBER=$Line
+    echo ""
+    echo "Running ${RUNNUMBER}"
+    echo ""		
+    echo ""
+    
+    if [[ -f "/group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/OUTPUT/${RUNNUMBER}.root" ]]; then  # check does file exist
+	cd /group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/OUTPUT/	
+	rm "${RUNNUMBER}.root"
+	echo "An old file exist, now it has been deleted !!!"
+	
+	cd /group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/
+#	eval "root -l -q \"/group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/Analysed.C("${RUNNUMBER}")\""
+	root -b -q  "Analysed.C(\"${RUNNUMBER}\")"
+    else
+	cd /group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/
+#	eval "root -l -q \"/group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/Analysed.C("${RUNNUMBER}")\""
+	root -b -q "Analysed.C(\"${RUNNUMBER}\")"
+    fi
+    
+done < "/group/c-kaonlt/USERS/${USER}/hallc_replay_lt/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/RunListHMSCalEff"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/tmp"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2center_highe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left1_highe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2center_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left1_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left2_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_Summer19_All"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_2p7"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_3p6"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_4p5"
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    runNum=$line
+    RootName1+="{runNum}.root "
+    # Change this path to match where your script is trying to look for files
+    #if [ ! -f "${UTILPATH}/OUTPUT/Analysis/ProtonLT/${runNum}_-1_Analysed_Data.root" ]; then
+    # Change this warning to be relevant/useful for you
+    #   echo "Proton analysis not found for run $runNum in ${UTILPATH}/OUTPUT/Analysis/ProtonLT/"
+    #  TestingVar=$((TestingVar+1))
+    #cd ${REPLAYPATH}/UTIL_KAONLT/scripts/HeepSingleStudy/SHMS_Heep/
+
+done < "/group/c-kaonlt/USERS/${USER}/hallc_replay_lt/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/RunListHMSCalEff"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/tmp"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2center_highe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left1_highe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2center_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left1_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/Q0p375W2p2left2_lowe"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_Summer19_All"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_2p7"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_3p6"
+#done < "${REPLAYPATH}/UTIL_BATCH/InputRunLists/KaonLT_2018_2019/HeeP_Coin_4p5"
+
+echo ""
+
+echo ""
+echo "Processing is completed" 
+exit 0 
---
+++ LTsep/Analysis/run_batch.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+echo "Running as ${USER}"
+
+if [[ $1 -eq "" ]]; then
+    MAXEVENTS=-1
+else
+    MAXEVENTS=$1
+fi
+
+##Output history file##
+historyfile=hist.$( date "+%Y-%m-%d_%H-%M-%S" ).log
+
+##Output batch script##
+batch="${USER}_Job.txt"
+
+Workflow="LTSep_${USER}"
+
+while true; do
+    read -p "Do you wish to begin a new batch submission for physics analysis? (Please answer yes or no) " yn
+    case $yn in
+        [Yy]* )
+            i=-1
+            cp /dev/null ${batch}
+            ##Creation of batch script for submission##
+            echo "PROJECT: c-kaonlt" >> ${batch}
+            echo "TRACK: analysis" >> ${batch}
+            echo "JOBNAME:physics" >> ${batch}
+            # Request disk space depending upon raw file size
+            
+	    # echo "DISK_SPACE: "$(( $TapeFileSize * 3 ))" GB" >> ${batch}
+	    #  if [[ $TapeFileSize -le 45 ]]; then
+	    #	echo "MEMORY: 4000 MB" >> ${batch}
+	    #    elif [[ $TapeFileSize -ge 45 ]]; then
+	    #		echo "MEMORY: 6000 MB" >> ${batch}
+	    #	    fi
+
+	    echo "OS: centos7" >> ${batch}
+	    echo "OS: general" >> ${batch}
+            echo "CPU: 1" >> ${batch} ### hcana single core, setting CPU higher will lower priority!              
+	    #	    echo "INPUT_FILES: ${tape_file}" >> ${batch}
+	    #echo "TIME: 1" >> ${batch} 
+	    echo "COMMAND:/group/c-kaonlt/USERS/vijay/lt_analysis/LTsep/Analysis/RunScript.sh" >> ${batch}
+	    echo "MAIL: ${USER}@jlab.org" >> ${batch}
+            echo "Submitting batch"
+	    #                eval "jsub ${batch} 2>/dev/null"
+	    eval "/site/bin/swif2 add-jsub ${Workflow} -script ${batch} 2>/dev/null"
+            echo " "
+            i=$(( $i + 1 ))
+	    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	    echo " "
+	    echo "###############################################################################################################"
+	    echo "############################################ END OF JOB SUBMISSIONS ###########################################"
+	    echo "###############################################################################################################"
+	    echo " "
+	    
+	    eval 'swif2 run ${Workflow}'
+	    break;;
+        [Nn]* ) 
+	    exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
